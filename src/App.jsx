6e08dc0a52353ba12d326d424f73c8f22d92e3b1
Aniket Yadav <yadavaniket7611@gmail.com>
2025-05-30 21:14:36 +0530

    added the necessary comments in the files

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import MovieCard from './Components/MovieCard';
 import { useDebounce } from 'react-use';
 import { getTrendingMovies, updateSearch } from './appwrite';
 
-
+// Base Url of the movie website
 const API_BASE_URL ='https://api.themoviedb.org/3'
+// accessing the api_key from the env file
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
-
+// object to make api request
 const API_OPTIONS = {
   method: 'GET',
   headers: {
@@ -19,36 +20,52 @@ const API_OPTIONS = {
   }
 };
 const App = () => {
+  // state for debouncing of search function
   const [debouncedSearchTerm,setDebouncedSearchTerm] = useState("")
+
   const [errorMessage, setErrorMessage] = useState('')
+  // state in which the movies fetched from the apis are stored
   const [movieList,setMovieList] = useState([])
+  // state in which teh trending movies are stored
   const [trendingMovies,setTrendingMovies] = useState([])
 
   const [isLoading,setIsLoading] = useState(false)
+  // state for getting the search term or query
   const [searchTerm, setSearchTerm] = useState("")
 
+  // useDebounce hook to implement the debouncing of the search 
   useDebounce(() => setDebouncedSearchTerm(searchTerm),500,[searchTerm])
+
+  // function in which we fetch the movies
   const fetchMovies = async (query ='')=> {
     setIsLoading(true)
     setErrorMessage('')
     try {
+      // if the query is present then the endpoint will be to search the movie according to the particular query
       const endPoint = query? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` :
+      // endpoint to fetch the movies by popularity
       `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      // variable to store the response the fetch
       const response = await fetch(endPoint,API_OPTIONS)
-      console.log("Status code:", response.status);
+      
+
       if(!response.ok){
         throw new Error("FAIL HOGYA FETCH KRNE M!!")
       }
-
+      // variable to store the response after converting it into an json
       const data = await response.json()
       if(data.Response ==='false'){
         setErrorMessage(data.Error || "failed to fetch the data")
         setMovieList([])
         return;
       }
+      // setting the movieList with the results(that's what we need) in the data that we got
       setMovieList(data.results || [])
-
+      // console.log(data)
+      // console.log(data.results)
+      
     if(query && data.results.length > 0){
+      // calling the updateSearch function to update the query in the appwrite database
       await updateSearch(query,data.results[0]);
   }
     } catch (error) {
@@ -58,22 +75,29 @@ const App = () => {
       setIsLoading(false)
     }
   }
+  // Function to load trending movies
   const loadTrendingMovies = async()=>{
-    try {
-      const movies = await getTrendingMovies()
 
+    try {
+      // storing the array that we got from the getTrendingMovies function in the movies variables
+      const movies = await getTrendingMovies()
+      // setting the trending movies to movies
       setTrendingMovies(movies)
     } catch (error) {
         console.log(error)
     }
   }
+  // useEffect hook to fetch the movies with a query
   useEffect(()=> {
+    // calling the fetchMovies function with a query to use the search endpoint
     fetchMovies(debouncedSearchTerm)
 
   },[debouncedSearchTerm])
-  
+  // UseEffect to load the trending moveis 
   useEffect(() => {
+    
     loadTrendingMovies()
+    // no dependency array because we only have to call it when the website loads 
   },[])
   return (
     <main>
@@ -88,7 +112,6 @@ const App = () => {
         {trendingMovies.length > 0 && (
           <section className="trending">
             <h2>Trending Movies</h2>
-
             <ul>
               {trendingMovies.map((movie, index) => (
                 <li key={movie.$id}>
--- src/appwrite.js
+++ src/appwrite.js
@@ -1,30 +1,37 @@
 import { Client, Databases, ID, Query } from 'appwrite'
+
+// getting the ids from the env file and storing all the ids of appwrite in new variables
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID
 const DATABASE_ID= import.meta.env.VITE_APPWRITE_DATABASE_ID
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID
 
 
-
+// creating a new client object for the appwrite
 const client = new Client()
+    // Url of the appwrite server
     .setEndpoint("https://fra.cloud.appwrite.io/v1")
+    // Setting the project id(id for the project we are working on)
     .setProject(PROJECT_ID)
 
+// getting the access to the database of that particular project
 const database = new Databases(client)
 
-
+// function to update the count in the db of the no. of times a movie is searched 
 export const updateSearch = async(searchTerm, movie) => {
     
     try {
+        //getting the query that is searched from the db if it exists
         const result = await database.listDocuments(DATABASE_ID,COLLECTION_ID,[
             Query.equal('searchTerm',searchTerm),
         ])
-
+        // if the movie is already present in teh document then just increase it's count
         if(result.documents.length>0){
             const doc = result.documents[0];
             await database.updateDocument(DATABASE_ID,COLLECTION_ID, doc.$id, {
                 count: doc.count+1
             })
         }
+        // if that searchTerm or movie doesn't exists then just add it in the db and make its count to 1
         else {
             await database.createDocument(DATABASE_ID,COLLECTION_ID,ID.unique(),{
                 searchTerm,
@@ -38,6 +45,8 @@ export const updateSearch = async(searchTerm, movie) => {
     }
 }
 
+
+// function to get the top 5 movies from the db
 export const getTrendingMovies = async () => {
     try {
         const result = await database.listDocuments(DATABASE_ID,COLLECTION_ID,[
